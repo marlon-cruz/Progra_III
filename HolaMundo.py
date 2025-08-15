6645e08fd87c0756b54f9842f1e6c971c6a66ae2
Marlon.C <USTR025024@ugb.edu.sv>
2025-08-15 09:47:36 -0600

    Ejercicio 1  de mayor o menor

---
+++ HolaMundo.py
@@ -0,0 +1,3 @@
+nombre = input("Hola dime cual es tu nombre?: ")
+
+print("Bienvenido a la clase de programación 3 ", nombre)
---
+++ mayor.py
@@ -0,0 +1,10 @@
+num1 = float(input("Ingrese el primer numero para la comparación: "))
+num2 = float( input("Ingrese el segundo numero para la comparación: "))
+
+
+if num1 > num2:
+    print("El mayor que ", num1)
+elif num1 < num2:
+    print("El mayor que ", num2)
+else:
+    print("Ingreso el mismo numero en las 2 opciones")
