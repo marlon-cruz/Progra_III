5d514239b21a2010ea4fe8c97315ea469be113b5
Marlon.C <USTR025024@ugb.edu.sv>
2025-08-22 09:54:21 -0600

    funcion de resta

--- lista01.py
+++ lista01.py
@@ -2,12 +2,11 @@ print("----------------------------")
 print("-------Lista de datos-------")
 print("----------------------------")
 print("----------------------------")
-num1 = int(input("---Ingrese un numero de 1 a 100: "))
-list = list(range(1,num1 +1))
-
+num1 = int(input("Ingrese un numero de 1 a 100: "))
+lista = list(range(1,num1 +1))
 if num1 > 100 or num1 < 1:
     print("El numero debe estar entre 1 y 100")
 else:
-    result = sum(list)
-    print(f"La suma de la lista gasta {num1} es {result}")
+    result = sum(lista)
+    print(f"La suma de la lista hasta {num1} es {result}")
 
