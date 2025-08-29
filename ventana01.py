efae54b8902ad9f6d9947cb66dd8c25b0d7a2db7
Marlon.C <USTR025024@ugb.edu.sv>
2025-08-29 09:02:12 -0600

    Formulario basico

---
+++ ventana01.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+class Aplication:
+    def __init__(self):
+        formulario01 = tk.Tk()
+        formulario01.title('Form 1')
+        formulario01.mainloop()
+
+
+
+llamada = Aplication()
